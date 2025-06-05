c0b31db33a81129c0e1b6003d843e6b0e5003870
Mônica Baldner <baldnermonica@gmail.com>
2025-06-05 19:46:28 -0300

    novas configurações

---
+++ src/test/java/com/algaworks/algaposts/ems_post_service/EmsPostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algaposts.ems_post_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsPostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
